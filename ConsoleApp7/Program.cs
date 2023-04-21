84488ef4307dc6a6cb28e0536cf38be613cb9c31
NuNes <NuNes@DESKTOP-RKLAV38>
2023-04-21 17:25:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.EmitirSom(); 
+
+            Cachorro cachorro = new Cachorro();
+            cachorro.EmitirSom(); 
+
+            Gato gato = new Gato();
+            gato.EmitirSom(); 
+
+            Console.ReadKey();
+
+        }
+    }
+}
